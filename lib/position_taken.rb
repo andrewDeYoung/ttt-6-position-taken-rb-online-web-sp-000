8e4871c77760240e8aec7e8b667a99ab9a711769
Andrew Shupe <adshupe@ucdavis.edu>
2018-12-10 18:59:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+  if board[index] == " " or "" or nil
+    return false
+  elsif board[index] == "X" or "O" 
+    return true 
+  end
+end
+
+
